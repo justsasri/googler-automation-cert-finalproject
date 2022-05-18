0cea53c2482378b88b2d2d217a9ca428bb5b43f9
sasriawesome <sasri.project@gmail.com>
2022-05-17 20:15:40 -0700

    finish health check.

--- emails.py
+++ emails.py
@@ -14,16 +14,17 @@ def generate(sender, recipient, subject, body, attachment_path=None):
   message["Subject"] = subject
   message.set_content(body)
 
-  # Process the attachment and add it to the email
-  attachment_filename = os.path.basename(attachment_path)
-  mime_type, _ = mimetypes.guess_type(attachment_path)
-  mime_type, mime_subtype = mime_type.split('/', 1)
+  if attachment_path is not None:
+    # Process the attachment and add it to the email
+    attachment_filename = os.path.basename(attachment_path)
+    mime_type, _ = mimetypes.guess_type(attachment_path)
+    mime_type, mime_subtype = mime_type.split('/', 1)
 
-  with open(attachment_path, 'rb') as ap:
-    message.add_attachment(ap.read(),
-                          maintype=mime_type,
-                          subtype=mime_subtype,
-                          filename=attachment_filename)
+    with open(attachment_path, 'rb') as ap:
+      message.add_attachment(ap.read(),
+                            maintype=mime_type,
+                            subtype=mime_subtype,
+                            filename=attachment_filename)
 
   return message
 
--- health_check.py
+++ health_check.py
@@ -9,26 +9,26 @@ import shutil
 import requests
 import emails
 
-def send_email(sender, title, body):
+def send_email(title):
   sender = "sender@example.com"
   receiver = "{}@example.com".format(os.environ.get('USER'))
   subject = title
-  message = emails.generate(sender, receiver, subject, body)
+  message = emails.generate(sender, receiver, subject, title + "\nPlease check your system and resolve the issue as soon as possible.")
   emails.send(message)
 
 def check_cpu():
     """ Report an error if CPU usage is over 80% """
     cpu = psutil.cpu_percent()
     if cpu >= 80:
-        pass
+        send_email("Error - CPU usage is over 80%")
     print("Checking CPU: {:.2f}%".format(cpu))
 
 def check_avalable_disk():
     """ Report an error if available disk space is lower than 20% """
     available_dist = psutil.disk_usage('/')
     if available_dist.percent <= 20:
-        pass
         current = available_dist.percent
+        send_email("Error - CPU usage is over 80%")
     print("Checking Disk: {:.2f}%".format(current))
 
 def check_memory():
@@ -36,7 +36,7 @@ def check_memory():
     mem = psutil.virtual_memory()
     mem_in_mega = mem.free / 1_000_000
     if mem.free <= mem_in_mega:
-        pass
+        send_email("Error - Available memory is less than 500MB")
     print("Checking RAM: {:.2f}%".format(mem_in_mega))
 
 def check_localhost_online():
@@ -45,7 +45,7 @@ def check_localhost_online():
         response = requests.get("http://localhost/")
         response.status_code
     except Exception as err:
-        print("request_dir")
+        send_email("Error - localhost cannot be resolved to 127.0.0.1")
 
 
 def main():
