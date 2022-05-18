b4a11635f2b8b4169cd53bcead62aee2c2c3e2d2
sasriawesome <sasri.project@gmail.com>
2022-05-17 18:46:51 -0700

    creaete heaalth check

---
+++ health_check.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+from cmath import exp
+import logging
+import os
+import sys
+import psutil
+import shutil
+import requests
+import emails
+
+def send_email(sender, title, body):
+  sender = "sender@example.com"
+  receiver = "{}@example.com".format(os.environ.get('USER'))
+  subject = title
+  message = emails.generate(sender, receiver, subject, body)
+  emails.send(message)
+
+def check_cpu():
+    """ Report an error if CPU usage is over 80% """
+    cpu = psutil.cpu_percent()
+    if cpu >= 80:
+        pass
+    print("Checking CPU: {:.2f}%".format(cpu))
+
+def check_avalable_disk():
+    """ Report an error if available disk space is lower than 20% """
+    available_dist = psutil.disk_usage('/')
+    if available_dist.percent <= 20:
+        pass
+        current = available_dist.percent
+    print("Checking Disk: {:.2f}%".format(current))
+
+def check_memory():
+    """ Report an error if available memory is less than 500MB """
+    mem = psutil.virtual_memory()
+    mem_in_mega = mem.free / 1_000_000
+    if mem.free <= mem_in_mega:
+        pass
+    print("Checking RAM: {:.2f}%".format(mem_in_mega))
+
+def check_localhost_online():
+    """ Report an error if the hostname "localhost" cannot be resolved to "127.0.0.1" """
+    try:
+        response = requests.get("http://localhost/")
+        response.status_code
+    except Exception as err:
+        print("request_dir")
+
+
+def main():
+    check_cpu()
+    check_memory()
+    check_avalable_disk()
+    check_localhost_online()
+
+if __name__ == "__main__":
+    main()
